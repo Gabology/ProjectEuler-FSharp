43310846c5144003e278fefeff76b16939909da4
Enfeathered <gabriel.a.svensk@gmail.com>
2014-10-30 18:11:20 +0100

    Changed to forward pipe :)

--- Problem1/Problem1/Program.fs
+++ Problem1/Problem1/Program.fs
@@ -1,11 +1,10 @@
 ﻿// Project Euler - Problem 1
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 // Find the sum of all the multiples of 3 or 5 below 1000.
-#light
 
 open System
 
 let multiples = [for n in 1..1000 do if n % 3 = 0 || n % 5 = 0 then yield n]
-let sum = List.sum multiples
-printfn "The sum of all multiples are: %d" sum
-ignore (Console.ReadKey ())
+             |> List.sum
+             |> printfn "The sum of all multiples are: %d"
+Console.ReadKey () |> ignore
