cac74fa990ebd6fbf55814296c8112c7f27e6059
Enfeathered <gabriel.a.svensk@gmail.com>
2014-11-05 22:22:15 +0100

    Solution 3 completed

--- Problem1/Problem3/Program.fs
+++ Problem1/Problem3/Program.fs
@@ -1,5 +1,17 @@
-﻿let problem2 =
-    let fibSeq = (1,1) |> Seq.unfold (fun (a, b) -> Some(a+b, (b, a+b)) )
-    fibSeq
-    |> Seq.takeWhile (fun x -> x <= 4000000)
-    |> Seq.sumBy (fun x -> if x%2 = 0 then x else 0)
+﻿// Project Euler - Problem 3
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143 ?
+open System
+open System.Diagnostics
+
+let p3 () = 
+    let rec primeFactors n i primes = 
+        if i > n/2L then n::primes else
+            let quotient, remainder = Math.DivRem(n, i)
+            if remainder = 0L then primeFactors quotient 2L (i::primes)
+            else primeFactors n (i + 1L) primes
+    primeFactors 600851475143L 2L []
+
+p3()
+|> List.head
+|> printf "The largest prime factor is: %d"
---
+++ Problem1/ScratchPad/Script.fsx
@@ -0,0 +1,34 @@
+﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#load "Library1.fs"
+open ScratchPad
+
+// Define your library scripting code here
+
+
+// Project Euler - Problem 3
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143 ?
+open System
+open System.Diagnostics
+
+let problem3 () = 
+    let rec primeFactors n i nums = 
+        if i > n/2L then n::nums else 
+            match Math.DivRem(n, i) with
+            | (quotient, 0L) -> primeFactors quotient 2L (i::nums)
+            | _ -> primeFactors n (i + 1L) nums
+    primeFactors 140L 2L []
+
+problem3()
+
+let p3 () = 
+    let rec primeFactors n i primes = 
+        if i > n/2L then n::primes else
+            let quotient, remainder = Math.DivRem(n, i)
+            if remainder = 0L then primeFactors quotient 2L (i::primes)
+            else primeFactors n (i + 1L) primes
+    primeFactors 600851475143L 2L []
+
+p3()
